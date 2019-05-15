21887c9bc388a18bd4cb9a889139e04a29e3c038
panjianyu <panjianyu@panjianyu.supcon.com>
2019-05-15 17:00:17 +0800

    some changes

--- src/org/utils/ad/ADInfo.java
+++ src/org/utils/ad/ADInfo.java
@@ -9,7 +9,7 @@ public class ADInfo {
 	public static final String ldapURL389 = "LDAP://10.10.51.222:389";// 用于普通操作
 	public static final String INITIAL_CONTEXT_FACTORY = "com.sun.jndi.ldap.LdapCtxFactory";
 	public static final String SECURITY_AUTHENTICATION = "simple";// LDAP访问安全级别："none","simple","strong"
-	public static final String ldapURL636 = "LDAP://10.10.51.222:636";// 用于密码操作
+	public static final String ldapURL636 = "LDAP://10.10.51.222:636";// 用于密码操作，端口不一样
 	public static final String factorysocket = "org.utils.ad.DummySSLSocketFactory";// 用于密码操作
 
 }
